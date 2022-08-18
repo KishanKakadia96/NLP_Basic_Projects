cb6f3a7a765b86ece88cabea066c04cf32d78264
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-18 15:52:29 +0200

    change mode

--- Text_to_speech/textToSpeech.py
+++ Text_to_speech/textToSpeech.py
@@ -5,6 +5,6 @@ def text2Speech(data):
     my_text = data
     tts = gTTS(text=my_text, lang='en', slow=False)
     tts.save('converted-file.mp3')
-    with open("converted-file.mp3", 'r') as file:
+    with open("converted-file.mp3", 'rb') as file:
         my_string = base64.b64encode(file.read())
     return my_string
