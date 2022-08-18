e184a27ffa9bf51039cf2f9b0cf1d1815c12fdfb
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-18 15:51:11 +0200

    Change the environment variable LANG

--- Text_to_speech/clientApp.py
+++ Text_to_speech/clientApp.py
@@ -3,6 +3,9 @@ import os
 from flask_cors import CORS, cross_origin
 import textToSpeech
 
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
 app = Flask(__name__)
 CORS(app)
 
