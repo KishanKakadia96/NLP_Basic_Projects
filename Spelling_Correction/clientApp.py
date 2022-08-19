4648e03357274f6f89bcde9c9662f2290d92da97
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-19 13:29:26 +0200

    main app with predict function

--- Spelling_Correction/clientApp.py
+++ Spelling_Correction/clientApp.py
@@ -2,18 +2,21 @@ from flask import Flask, render_template, request, jsonify
 import os
 from flask_cors import CORS, cross_origin
 
+from spellcorrector import spell_corrector
+
 app=Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
-cross_origin()
+@cross_origin()
 def home():
     return render_template('index.html')
 @app.route('/predict', methods=['POST'])
-cross_origin()
+@cross_origin()
 def predict():
-    pass
-
+    data = request.json['data']
+    result = spell_corrector(data)
+    return jsonify({"text": result})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
