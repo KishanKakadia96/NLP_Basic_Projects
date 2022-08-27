14e8b445d9553d5f7892b52ec4680f447b1898e9
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-27 23:50:50 +0200

    predict function created and saved model

--- Spam_detector/spams.py
+++ Spam_detector/spams.py
@@ -2,6 +2,10 @@ import os
 import io
 import numpy
 from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+#import pickle
+from sklearn.externals import joblib
 
 #This function to read the messages leaving the header from each of the files and suming it
 #to list for classification
@@ -51,4 +55,20 @@ classifier = MultinomialNB()
 targets = data1['class'].values
 classifier.fit(counts, targets)
 
+# saved model
+joblib.dump(classifier, 'spam.pkl')
 
+#Load pickle file
+class_from_joblib = joblib.load('spam.pkl')
+
+#Predicting the Spam Emails
+
+examples = ['Bitcoin, anyone?', "Hi Neil, how about a game of squash tomorrow?"]
+def prediction(example):
+    class_from_joblib = joblib.load('spam.pkl')
+    example_counts = vector.transform(example)
+    prediction = class_from_joblib.predict(example_counts)
+    return prediction
+
+prediction(examples)
+print(prediction)
