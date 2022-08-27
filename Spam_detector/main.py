d4208beedddb655b95a8e539e3526a9996b271f7
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-27 23:17:23 +0200

    Flask module created

---
+++ Spam_detector/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify,render_template
+import os
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=5000, debug=True)
