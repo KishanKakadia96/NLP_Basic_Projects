e69a78055c2d382f5f8fbe688fb010474769f5e0
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-27 23:27:56 +0200

    Reading file function created

---
+++ Spam_detector/spams.py
@@ -0,0 +1,24 @@
+import os
+import io
+import numpy
+from pandas import DataFrame
+
+#This function to read the messages leaving the header from each of the files and suming it
+#to list for classification
+
+def reading_Files(path):
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+
+            inBody = False
+            lines = []
+            f = io.open(path, 'r', encoding='latin1')
+            for line in f:
+                if inBody:
+                    lines.append(line)
+                elif line == '\n':
+                    inBody = True
+            f.close()
+            message = '\n'.join(lines)
+            yield path, message
